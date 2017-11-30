c7961ca0f4dd1a1a03a1b8b45a34c5c5263b211a
Aaron Rohrbacher and Keegan Ruebling <aaron@>
2017-11-29 16:24:30 -0800

    custom device routes for user profile and post sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
 
-
-
   resources :photos do
     member do
       put "like", to: "photos#upvote"
@@ -11,6 +9,9 @@ Rails.application.routes.draw do
     resources :tags
   end
 
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  match 'users/:id' => 'users#show', via: :get
 end
